06e953bcb80ee39c69fe49b171006883864bf250
user1600422967 <845975110037196-user1600422967@noreply.coze.cn>
2026-03-17 21:37:05 +0800

    Restored to 'd0537caa39204b1bc0fea03eefe1148dd03a4aeb'
    Coze-Restored-To: 'd0537caa39204b1bc0fea03eefe1148dd03a4aeb'
    Coze-Commit-Type: restore

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: '/shuiwumozhu',
+  // outputFileTracingRoot: path.resolve(__dirname, '../../'),  // Uncomment and add 'import path from "path"' if needed
+  /* config options here */
+  allowedDevOrigins: ['*.dev.coze.site'],
   images: {
-    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
